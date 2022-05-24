0d11a2fc8da298e312bdcfa3e600665e060e6fff
dieppa <hm.dieppa@gmail.com>
2022-05-25 00:59:22 +0200

    Removed EasyDI and BeaconBuilder entity as well

--- easydi/src/main/java/com/madapp/easydi/lib/EasyDI.kt
+++ easydi/src/main/java/com/madapp/easydi/lib/EasyDI.kt
@@ -2,19 +2,28 @@ package com.madapp.easydi.lib
 
 import java.lang.Exception
 
-object EasyDI {
-    val beacons: MutableMap<String, Beacon> = mutableMapOf()
-}
+val beacons: MutableMap<String, Beacon> = mutableMapOf()
+fun key(clazz: String, name: String) = "${clazz}__$name"
 
 inline fun <reified T : Any> register(
     name: String = String.empty(),
     singleton: Boolean = false,
-    noinline builder: () -> T) {
-    EasyDI.beacons["${T::class.java.name}_$name"] = BeaconBuilder.build(builder, singleton)
+    noinline creator: () -> T) {
+    val key = key(T::class.java.name, name)
+    beacons[key] = buildBeacon(creator, singleton)
 }
 
 inline fun <reified T> inject(name: String = String.empty()): T {
-    val beacon = EasyDI.beacons["${T::class.java.name}_$name"]
-        ?: throw Exception("No definition for ${T::class.java.name}")
+    val key = key(T::class.java.name, name)
+    val beacon = beacons[key]
+        ?: throw Exception("No definition for $key")
     return beacon.invoke() as T
+}
+
+fun buildBeacon(creator: () -> Any, singleton: Boolean): Beacon {
+    return if (singleton) {
+        SingleBeacon(creator)
+    } else {
+        Beacon(creator)
+    }
 }
